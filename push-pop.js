d35c3b74c76e66f16337c37e0bf2b058cda49b84
mostofa khan <mdmostofakhan1972@gmail.com>
2023-01-27 19:08:23 +0600

    conditionals

--- push-pop.js
+++ push-pop.js
@@ -1,3 +1,17 @@
 var numbers = [23, 56, 65, 454,34];
+// use push to add element to an array as the last
 numbers.push (59);
+// console.log(numbers);
+
+var friends = ['mim', 'sumiya', 'muskhan'];
+friends.push('gelam');
+friends.push('mostofa');
+console.log(friends);
+
+// use pop to get last element
 console.log(numbers);
+// numbers.pop();
+var elements = numbers.pop();
+console.log(numbers);
+console.log(elements);
+// console.log(numbers);
